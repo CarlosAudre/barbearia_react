b8926e28865fccd6930eee60d6f4174606d923e0
Carlos Audre Santos <carlosaudre180@gmail.com>
2026-08-03 00:25:03 -0300

    little bug

--- src/pages/adm/Service.jsx
+++ src/pages/adm/Service.jsx
@@ -190,7 +190,7 @@ export function Service() {
 
         {/* Update Service Form */}
         {updateServiceFormVisibility && (
-          <div className="absolute top-3/1 md:top/2/1 left-1/2 -translate-x-1/2 -translate-y-3/4  md:-translate-y-1/3">
+          <div className="absolute top-2/3 md:top/2/1 left-1/2 -translate-x-1/2 -translate-y-3/4  md:-translate-y-1/3">
             <NewServiceForm
               title="Atualizar Serviço"
               handleOnCancel={() =>
